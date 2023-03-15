e0b30c5feea8f26fea145e6c32e6eefa008519c9
Lucas <lucasmarinhopessoa14@gmail.com>
2023-03-14 21:20:47 -0300

    first commit

---
+++ perguntas.py
@@ -0,0 +1,83 @@
+perguntas = {
+    "1":["Asas mesotorácicas espessas, fortemente eclerotizadas ou pergaminácias, pelo menos na base, ou vestigiais.",
+         "Asas mesotorácicas membranosas, às vezes cobertas com escamas, nunca vestigiais."],
+
+    "2":["Asas mesotorácicas vestigiais, em forma de escamas ou claviformes. Asas posteriores grandes, em forma de leque ",
+         "Asas mesotorácicas cobrem a metade ou mais do abdômen; nunca claviformes ou em forma de escamas."],
+
+    "3":["Abdômen com cercos grandes, fortemente esclerotizados e em forma de pinça, asas anteriores curtas, deixando pelo menos 3 segmentos abdominais expostos",
+         "Abdômen com cercos ausentes ou não em forma de pinça; asas posteriores normamemnte cobrem todo o abdômen"],
+
+    "4":["As peças bucais formam um rostro fino e alongado, dobrado na base, que se projeta para baixo e para trás da cabeça.",
+         "Peças bucais curtas, com mandíbulas, cabeça às vezes prolongada como um bico, mas nunca com a base dobrada."],
+
+    "5":["Asas anteriores sem nervação, em geral fortemente esclerotizadas e se encontram no meio quando em repouso; antenas raramente com mais de 11 segmentos, frequentemente claviformes.",
+         "Asas anteriores com muitas nervuras (veias) e reticulada; antenas filiformes, nunca claviformes, normalmente com mais de 12 segmentos."],
+
+    "6":["Pernas posteriores com fêmures grandemente aumentados para saltar",
+         "Pernas posteriores não modificadas, semelhantes às pernas medianas "],
+
+    "7":["Pernas anteriores raptórias.",
+         "Pernas anteriores não raptóriais, próprias para caminhar."],
+
+    "8":["Cabeça prognata (peças bucais dirigidas para frente); corpo em forma de graveto ou semelhante a folha.",
+         "Cabeça hipognata (peças bucais dirigidas para baixo); corpo não em forma de graveto ou folha."],
+
+    "9":["Pernas posteriores em fêmures aumentados para saltar; protórax projetando-se para trás sobre as asas e o abdômen.",
+         "Pernas posteriores não modificadas para saltar, semelhantes às pernas medianas; protórax pequeno, não se projetando sobre a parte posterior do corpo."],
+
+    "10":["Um par de asas presente.",
+          "Dois pares de asas."],
+
+    "11":["Abdômen com 1 a 3 filamentos longos no segmento terminal; peças bucais vestigiais.",
+          "Abdômen sem filamentos longos; peças bucais raramente vestigiais "],
+
+    "12":["Antenas longas, filiformes; asas com uma única veia sem células.",
+          "Antenas curtas, em forma de cerda; asas com células fechadas, normalmente com numerosas veias"],
+
+    "13":["Abdômen com 2 a 3 filamentos terminais longos",
+          "Abdômen sem filamentos terminais ou com processos muito curtos"],
+
+    "14":["Tarsos com 1 a 4 segmentos.",
+          "Tarsos com 5 segmentos."],
+
+    "15":["Peças bucais formam um rostro em forma de tubo fino, que se projeta para baixo da cabeça.",
+          "Peças bucais curtas e com mandibulas, nunca um rostro longo que se projeta."],
+
+    "16":["Pernas anteriores com segmento basal no tarso globoso, pelo menos duas vezes a espessura do segundo segmento.",
+          "Pernas anteriores com segmentos tarsais aproximadamente do mesmo tamanho."],
+
+    "17":["Antenas mais curtas que a cabeça, em forma de cerda, insetos com pelo menos 1.5cm de comprimento; abdômen longo e esbelto; asas com muitas veias, em forma de rede.",
+          "Antenas filiformes e mais longas que a cabeça."],
+
+    "18":["Tarso com 4 segmentos.",
+          "Tarsos com 2 a 3 segmentos."],
+
+    "19":["Último segmento de abdômen com cercos; insetos muito pequenos que ocorrem em madeira em decomposição",
+          "Cercos Ausentes."],
+
+    "20":["Asas lineares, estreitas, com no máximo 2 veias; cabeça em forma de cone, dirigida para baixo ou para trás.",
+          "Asas Ovais, com pelo menos 4 veias longitudinais "],
+
+    "21":["Asas Posteriores maiores que as anteriores; peças bucais com mandíbula, que quase sempre bem desenvolvidas.",
+          "Asas anteriores maiores que as posteriores, visivelmente triangulares; peças bucais vestigiais."],
+
+    "22":["Asas anteriores densamente cobertas com pelos ou escamas.",
+          "Asas desprovidas de pelos ou escamas, ou com franjas de pelos marginais."],
+
+    "23":["Asas cobertas com escamas; peças bucais usualmente em forma de uma probóscide enrolada em espiral.",
+          "Asas anteriores cobertas com pelos; peças bucais com mandíbulas."],
+
+    "24":["Asas anteriores em torno de 1,5 vezes mais longas que as asas posteriores; asas anteriores e posteriores normalmente bem diferentes tanto na forma quanto na venação; abdômen normalmente com forte constrição.",
+          "Asas anteriores e posteriores semelhantes no tamanho, na forma e na venação; abdômen não constrito na base."],
+
+    "25":["Cabeça não prolongada ventralmente; asas usualmente asas usualmente com numerosas veias transversais na margem costal (exceção Coniopterygidae).",
+          "Cabeça prolongada ventralmente, em forma de bico; asas com 1 a 3 veias transversais na margem costal."],
+
+    "26":["Asas posteriores com base mais larga que das asas anteriores; veias não bifurcam próximo á margem da asa.",
+          "Asas posteriores com largura na vase menor que a das ansas anteriores; pelo menos veias radiais bifurcam um pouco antes da margem das asas (exceção: Coniopterygidae )."],
+
+    "27":["Pronoto quadrado ou quase quadrado; asas sem pterostigma.",
+          "Pronoto pelo menos 3 vezes mais longo que largo, asas com pterostigma."],
+    "28":["",""]
+}
---
+++ run.py
@@ -0,0 +1,247 @@
+from flask import Flask, render_template, redirect, session, request
+from perguntas import perguntas
+import wikipedia
+wikipedia.set_lang('pt')
+
+app = Flask(__name__)
+app.secret_key = 'secret_key'
+
+questao = 1
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    global questao
+    pergunta1 = perguntas[f"{questao}"][0]
+    pergunta2 = perguntas[f"{questao}"][1]
+    return render_template('teste.html', pergunta1=pergunta1, pergunta2=pergunta2)
+
+@app.route('/resposta', methods=["POST"])
+def resposta():
+    global questao
+    global ordem   
+    selected_option = request.form.get("flexRadioDefault")
+    match questao:
+        case 1:
+            if selected_option == "pergunta1":
+                questao = 2
+            if selected_option == "pergunta2":
+                questao = 10
+
+        case 2:
+            if selected_option == "pergunta1":
+                    questao = 9
+            if selected_option == "pergunta2":
+                    questao = 3
+
+        case 3:
+            if selected_option == "pergunta1":
+                    ordem = 'Dermaptera'
+                    questao = 28
+            if selected_option == "pergunta2":
+                    questao = 4
+
+        case 4:
+
+            if selected_option == "pergunta1":
+                ordem = 'Hemiptera'
+                questao = 28
+            if selected_option == "pergunta2":
+                questao = 5
+
+        case 5:
+            if selected_option == "pergunta1":
+                    ordem = 'Coleoptera'
+                    questao = 28
+            if selected_option == "pergunta2":
+                    questao = 6
+
+        case 6:
+            if selected_option == "pergunta1":
+                    ordem = 'Orthoptera'
+                    questao = 28
+            if selected_option == "pergunta2":
+                    questao = 7
+
+        case 7:
+            if selected_option == "pergunta1":
+                    ordem = 'Mantodea'
+                    questao = 28
+            if selected_option == "pergunta2":
+                    questao = 8
+
+        case 8:
+            if selected_option == "pergunta1":
+                    ordem = 'Phasmatodea'
+                    questao = 28
+            if selected_option == "pergunta2":
+                    ordem = 'Blattodea'
+                    questao = 28
+
+        case 9:
+            if selected_option == "pergunta1":
+                    ordem = 'Orthoptera'
+                    questao = 28
+            if selected_option == "pergunta2":
+                    ordem = 'Strepsiptera'
+                    questao = 28
+
+        case 10:
+            if selected_option == "pergunta1":
+                    questao = 11
+            if selected_option == "pergunta2":
+                    questao = 13
+
+        case 11:
+            if selected_option == "pergunta1":
+                    questao = 12
+            if selected_option == "pergunta2":
+                    ordem = 'Diptera'
+                    questao = 28
+
+        case 12:
+            if selected_option == "pergunta1":
+                    ordem = 'Hemiptera'
+                    questao = 28
+            if selected_option == "pergunta2":
+                    ordem = 'Ephemeroptera'
+                    questao = 28
+
+        case 13:
+            if selected_option == "pergunta1":
+                    questao = 21
+            if selected_option == "pergunta2":
+                    questao = 14
+
+        case 14:
+            if selected_option == "pergunta1":
+                    questao = 15
+            if selected_option == "pergunta2":
+                    questao = 22
+
+        case 15:
+            if selected_option == "pergunta1":
+                    ordem = 'Hemiptera'
+                    questao = 28
+            if selected_option == "pergunta2":
+                    questao = 16
+
+        case 16:
+            if selected_option == "pergunta1":
+                    ordem = 'Embiidina'
+                    questao = 28
+            if selected_option == "pergunta2":
+                    questao = 17
+
+        case 17:
+            if selected_option == "pergunta1":
+                    ordem = 'Odonata'
+                    questao = 28
+            if selected_option == "pergunta2":
+                    questao = 18
+
+        case 18:
+            if selected_option == "pergunta1":
+                    ordem = 'Isoptera'
+                    questao = 28
+            if selected_option == "pergunta2":
+                    questao = 19
+
+        case 19:
+            if selected_option == "pergunta1":
+                    ordem = 'Zoraptera'
+                    questao = 28
+            if selected_option == "pergunta2":
+                    questao = 20
+
+        case 20:
+            if selected_option == "pergunta1":
+                    ordem = 'Thysanopetera'
+                    questao = 28
+            if selected_option == "pergunta2":
+                    ordem = 'Pscopetera'
+                    questao = 28
+
+        case 21:
+            if selected_option == "pergunta1":
+                    ordem = 'Plecoptera'
+                    questao = 28
+            if selected_option == "pergunta2":
+                    ordem = 'Ephemeroptera'
+                    questao = 28
+
+        case 22:
+            if selected_option == "pergunta1":
+                    questao = 23
+            if selected_option == "pergunta2":
+                    questao = 24
+
+        case 23:
+            if selected_option == "pergunta1":
+                    ordem = 'Lepidoptera'
+                    questao = 28
+            if selected_option == "pergunta2":
+                    ordem = 'Trichoptera'
+                    questao = 28
+
+        case 24:
+            if selected_option == "pergunta1":
+                    ordem = 'Hymenoptera'
+                    questao = 28
+            if selected_option == "pergunta2":
+                    questao = 25
+
+        case 25:
+            if selected_option == "pergunta1":
+                    questao = 26
+            if selected_option == "pergunta2":
+                    ordem = 'Mecoptera'
+                    questao = 28
+
+        case 26:
+            if selected_option == "pergunta1":
+                    questao = 27
+            if selected_option == "pergunta2":
+                    ordem = 'Neuropetera'
+                    questao = 28
+
+        case 27:
+            if selected_option == "pergunta1":
+                    ordem = 'Megaloptera'
+                    questao = 28
+            if selected_option == "pergunta2":
+                    ordem = 'Raphidoptera'
+                    questao = 28
+    if questao == 28:
+        return redirect('ordem_inseto')
+    else:
+        return redirect('/')
+
+@app.route('/zerar', methods=["POST","GET"])
+def zerar():
+    global questao
+    questao = 1
+    return redirect('/')
+
+@app.route('/sobre')
+def sobre():
+    return render_template('sobre.html')
+
+@app.route('/ordem_inseto')
+def ordem_inseto():
+    global ordem
+    def returnimage(pagina_inseto):
+        check = True
+        cont = 0
+        while(check):
+            if(('jpg' in str(pagina_inseto.images[cont]) or 'png' in str(pagina_inseto.images[cont]))):
+                check = False
+            else:
+                cont = cont + 1
+        return pagina_inseto.images[cont]
+    pagina_inseto = wikipedia.page(ordem)
+    imagem_inseto = returnimage(pagina_inseto)
+    descricao_inseto = wikipedia.summary(ordem)
+    return render_template('ordem.html', descricao_inseto=descricao_inseto, ordem = ordem, imagem_inseto = imagem_inseto)
+
+if __name__ == "__main__":
+    app.run(debug=True)
